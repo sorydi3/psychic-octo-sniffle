5a8e19f1efa4ef555acef1dff8910e6e5850ad9f
sorydi3 <ibrahimasory22@hotmail.com>
2021-05-27 10:09:14 +0100

    updated batut

---
+++ batuts.pl
@@ -0,0 +1,38 @@
+establiment(
+    best_batuts, [alan,john,mary],
+    [
+        batut(berry, [orange, blueberry, strawberry], 2),
+        batut(tropical, [orange, banana, mango, guava], 3),
+        batut(blue, [banana, blueberry], 3) 
+    ]
+).
+
+establiment(
+    all_batuts, [keith,mary],
+    [ 
+        batut(pinacolada, [orange, pineapple, coconut], 2),
+        batut(green, [orange, banana, kiwi], 5),
+        batut(purple, [orange, blueberry, strawberry], 2),
+        batut(smooth, [orange, banana, mango],1) 
+    ]
+).
+
+establiment(
+    batuts_galore, [heath,john,michelle],
+    [ 
+        batut(combo1, [strawberry, orange, banana], 2),
+        batut(combo2, [banana, orange], 5),
+        batut(combo3, [orange, peach, banana], 2),
+        batut(combo4, [guava, mango, papaya, orange],1),
+        batut(combo5, [grapefruit, banana, pear],1) 
+    ]
+).
+
+
+
+mesDe(N,E):-true.
+elFa(B,E):-true.
+ratio(E,R):-true.
+promig(E,P):-true.
+mesbarat(E):-true.
+trobaBatuts(L,D,I):-true. 
